837e09970205d8f83f3f04b9ca1b52579ddcd12c
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2025-02-11 19:10:31 +0300

    setup jococo, add new test for 100% coverage

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -3,7 +3,7 @@ package ru.netology.statistic;
 public class StatisticsService {
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
-        for (long income : incomes) {
+        for (long income : incomes) {  // {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
             if (currentMax < income) {
                 currentMax = income;
             }
